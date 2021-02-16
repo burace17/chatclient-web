c948925f6ea01efd3f3865566e55c011b5384c1b
Blair Urish <burace17@gmail.com>
2021-02-15 17:26:54 -0800

    Sort user list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
- 
+
 import "./UserList.css";
 import "./util.css";
 import { User, UserStatus } from "../net/client";
@@ -11,33 +11,43 @@ interface Properties {
     users: User[]
 }
 
+function addUser(user: User) {
+    let className;
+    switch (user.status) {
+        case UserStatus.Online:
+            className = "online-user";
+            break;
+        case UserStatus.Away:
+            className = "away-user";
+            break;
+        case UserStatus.Offline:
+            className = "offline-user";
+            break;
+    }
+
+    return (
+        <li key={user.id}>
+            <span className={className}>{user.nickname}</span>
+        </li>
+    );
+}
+
+function compareUser(a: User, b: User) {
+    if (a.status === UserStatus.Online && b.status !== UserStatus.Online)
+        return -1;
+    else if (a.status !== UserStatus.Online && b.status === UserStatus.Online)
+        return 1;
+    else
+        return a.nickname.localeCompare(b.nickname);
+}
+
 function UserList(props: Properties) {
     const name = props.isHidden ? "scrollbar user-list hidden" : "scrollbar user-list";
-    const addUser = (user: User) => {
-        let className;
-        switch (user.status) {
-            case UserStatus.Online:
-                className = "online-user";
-                break;
-            case UserStatus.Away:
-                className = "away-user";
-                break;
-            case UserStatus.Offline:
-                className = "offline-user";
-                break;
-        }
-
-        return (
-            <li key={user.id}>
-                <span className={className}>{user.nickname}</span>
-            </li>
-        );
-    };
 
     return (
         <div className={name}>
             <ul>
-                {props.users.map(addUser)}
+                {props.users.sort(compareUser).map(addUser)}
             </ul>
         </div>
     );
